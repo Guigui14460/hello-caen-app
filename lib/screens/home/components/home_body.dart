c60d8e1d2f4a455b91f8322346b8feaf048e5b89
myrani <myrani@protonmail.com>
2021-01-23 03:43:02 +0100

    Début de solution à la bar de Spécialisation avec ListView

--- lib/screens/home/components/home_body.dart
+++ lib/screens/home/components/home_body.dart
@@ -29,11 +29,21 @@ class _HomeBodyState extends State<HomeBody> {
     return SafeArea(
       child: Scaffold(
           appBar: MyAppBar(),
-          body: Scaffold(
-            appBar : MyAppBar(),
-            body :SingleChildScrollView(
+          body: SingleChildScrollView(
               child:Column(
                 children: [
+                  SizedBox(height:50,width:500,child:
+                    ListView(
+                      children: [
+                        Container(width: 100,height: 50,color:Colors.white),
+                        Container(width: 100,height: 50,color:Colors.black),
+                        Container(width: 100,height: 50,color:Colors.white),
+                        Container(width: 100,height: 50,color:Colors.black),
+                        Container(width: 100,height: 50,color:Colors.white),
+                      ],
+                      scrollDirection: Axis.horizontal,
+                    ),
+                  ),
                   Container(
                     width: MediaQuery.of(context).size.width * 0.99,
                     height: MediaQuery.of(context).size.width * 0.8,
@@ -41,6 +51,7 @@ class _HomeBodyState extends State<HomeBody> {
                     child: Container(
                       child :Column(
                           children: [
+
                             SizedBox(height: 10),
                             Row( children: [
                               Container(
@@ -141,7 +152,6 @@ class _HomeBodyState extends State<HomeBody> {
             ),
           )
         )
-      )
       );
 
   }

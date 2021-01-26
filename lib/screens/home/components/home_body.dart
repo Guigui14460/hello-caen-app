42a63b77d9a0fb6ec4c63e15f5102ba4bfe32246
myrani <myrani@protonmail.com>
2021-01-26 01:52:37 +0100

    First Approch of the Auto Generated Content bar

--- lib/screens/home/components/home_body.dart
+++ lib/screens/home/components/home_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_caen/components/category_menu.dart';
 import 'package:provider/provider.dart';
 
 import 'account_profile.dart';
--- lib/screens/home/components/store_list.dart
+++ lib/screens/home/components/store_list.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hello_caen/components/category_menu.dart';
 import  "package:hello_caen/model/database/commerce_model.dart";
+import 'package:hello_caen/screens/location/location_screen.dart';
 class StoreListPage extends StatelessWidget {
   const StoreListPage({Key key}) : super(key: key);
-
+  static dosmth(param){print(param);}
   @override
   Widget build(BuildContext context) {
     List<Widget> widgets = [];
+    return SafeArea(child: CategoryMenu(text: ["B1","B2","B3","B4"],onPressed :[ dosmth("click 1"),dosmth("click 2"),dosmth("3"),dosmth("3") ]));
+
     return SafeArea(child: FutureBuilder(
         future :  CommerceModel().getAll(),
         builder: (context,snapshot){
@@ -15,9 +19,8 @@ class StoreListPage extends StatelessWidget {
               print(snapshot.data);
               print("--------------------------------------------------------------");
               for (var data in snapshot.data) {
-                widgets.add(Text(data.id));
+                widgets.add(Text(data.name));
               }
-              widgets.add(Text("sweet sweet sanity"));
               return ListView.builder(
                   itemCount: widgets.length,
                   itemBuilder: (context, index) {

babaa42fa3c961933f35caa10e2d6ca7944da0dc
myrani <myrani@protonmail.com>
2021-01-28 20:54:55 +0100

    Final Draft for first design

--- lib/components/caller_row.dart
+++ lib/components/caller_row.dart
@@ -15,52 +15,75 @@ class _CallerRowState extends State<CallerRow> {
       return Container(
           width: 700,
           height: 200,
-          color: Colors.amber,
+          //color: Colors.amber,
+          margin: EdgeInsets.only(top:40),
+          child:
+              FutureBuilder(
+                future: CommerceModel().getAll(),
+                builder: (context, snapshot) {
+                if(snapshot.connectionState == ConnectionState.done){
+                  List<Widget> widgets = [];
+                  for(var data in snapshot.data){
+                    widgets.add(
+                        GestureDetector(onTap: () {print(data.name);} ,
+                            child:Column(
+                                children: [Container(
+                                //margin:EdgeInsets.all(5.0),
+                                width: 250,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                  data.imageLink),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.all(Radius.circular(15))),
+                            ),
+                            Row(children: [Container(
+                              width: 75,
+                              height: 75,
+                              margin: EdgeInsets.only(top:10.0,left:10),
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                        data.imageLink),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(15))),
+                            ),Container(
+                              width: 150,
+                              height: 100,
+                              //color: Colors.black,
+                              child:Column(
+                                children: [
+                                  Container(width:90,height:20 ,margin: EdgeInsets.only(top:25),padding: EdgeInsets.only(left:10) ,child: Text(data.name),),
+                                  Container(width:90,height:20 ,margin: EdgeInsets.only(top:5),child: Opacity(opacity:0.5, child: Text(data.name)),),
+                                  Container(width:90,height:20 ,margin: EdgeInsets.only(top:5),child: Opacity(opacity:0.5, child: Text(data.name)),),
+                                ],
+                              )
+                              //borderRadius: BorderRadius.all(Radius.circular(15))),
+                            )],)
 
-          child:FutureBuilder(
-            future: CommerceModel().getAll(),
-            builder: (context, snapshot) {
-            if(snapshot.connectionState == ConnectionState.done){
-              List<Widget> widgets = [];
-              for(var data in snapshot.data){
-                widgets.add(Container(
-                    margin:EdgeInsets.all(5.0),
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              data.imageLink),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    child:Container(
-                        child:Opacity(opacity: 0.5,child:Container(padding:EdgeInsets.all(5.0),color:Colors.black,child:Text(data.name))))
-                  )
-                );
 
-
+                            ])
+                ));
+              widgets.add(SizedBox(width: 20,));
               }
 
               return ListView.builder(
                     scrollDirection : Axis.horizontal,
                     itemCount: widgets.length,
                     itemBuilder: (context,index){
-                      return widgets[index];
-                    }
-
-                  );
-
-
-
+                      return widgets[index];}
+                    );
             }
             else{
               return CircularProgressIndicator();
             }
 
 
-          })
-      );
+          }),
 
+      );
   }
 }
